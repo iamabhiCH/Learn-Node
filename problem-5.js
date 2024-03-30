b0d9a1ca75a2dc4b168d12c8e2044c8e066c02c0
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-30 15:45:36 +0530

    Problem 5th initialized.

---
+++ problem-5.js
@@ -0,0 +1,5 @@
+/*
+    You are working on a weather app. Create a JSON object representing the weather forecast for
+    a specific day. Include properties like "date," "temperature," "conditions," and "humidity."
+    And display the information.    
+*/
