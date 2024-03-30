af48ea54db363509ebf96e951c041c4cc7b1a268
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-30 15:45:12 +0530

    Problem 4th initialized.

---
+++ problem-4.js
@@ -0,0 +1,6 @@
+/*
+    You are organizing a party and want to track the list of guest. Create an array called
+    "guestList" and add the names of at least five guest. Then, WAP that checks if a given 
+    name is in the list or not? If  it is present then print “Welcome to the party ["name"]",
+    otherwise print “Sorry ["Name] is not invited”.
+*/
