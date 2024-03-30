db693508c66155271509967c379106339544d8dd
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-30 15:56:20 +0530

    Problem 4th completed successfully.

--- problem-4.js
+++ problem-4.js
@@ -4,3 +4,23 @@
     name is in the list or not? If  it is present then print “Welcome to the party ["name"]",
     otherwise print “Sorry ["Name] is not invited”.
 */
+
+const prompt = require("prompt-sync")();
+
+const guestList = ["Akash Sharma", "Akash Madhdheshiya", "Abhinav Arya", "Chandan Tiwari", "Roshan","Manish","Shashi","Awanish"];
+
+const guestName = prompt("Please enter your name : ");
+
+let flag = 0;
+
+for(let i = 0; i < guestList.length; i++){
+    if(guestList[i] == guestName){
+        flag = 1;
+    }
+}
+
+if(flag == 1){
+    console.log("Welcome to the party "+guestName);
+}else{
+    console.log("Sorry, "+guestName+" is not invited");
+}
