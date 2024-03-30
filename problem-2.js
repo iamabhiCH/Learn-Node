1913cf00e36b70ca6cd6c61e713594f5ccfe3d6a
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-30 13:30:25 +0530

    Problem 2nd completed.

--- problem-2.js
+++ problem-2.js
@@ -1,1 +1,10 @@
 // WAP to calculate the area of the rectangle  using user input for length and breadth and print output.
+
+const prompt = require("prompt-sync")();
+
+const l = parseInt(prompt("Enter the length of rectangle :"));
+const b = parseInt(prompt("Enter the breadth of rectangle :"));
+
+const area = l * b;
+
+console.log("Area of the rectangle : "+area);
