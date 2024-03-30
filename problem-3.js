d7f10e4bf3307d9934bca0b3e46d6710c3053a66
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-30 15:26:21 +0530

    Problem 3rd initialized.

---
+++ problem-3.js
@@ -0,0 +1,8 @@
+/*
+    Create a online store, define a onject named "product" with the following properties : 
+        - productName : (string)
+        - productPrice : (number)
+        - inStock : (boolean)
+    
+    create atleast three products using your object template and display their details.
+*/
